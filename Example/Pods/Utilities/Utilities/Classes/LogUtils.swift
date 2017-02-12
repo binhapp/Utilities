619f376d0dff658e865aef287b9969ae1a01570a
blcsntb <blcsntb@gmail.com>
2017-02-12 20:29:23 +0700

    update example for version 0.1.2

---
+++ Example/Pods/Utilities/Utilities/Classes/LogUtils.swift
@@ -0,0 +1,31 @@
+//
+//  LogUtils.swift
+//  Pods
+//
+//  Created by blcsntb on 11/30/16.
+//
+//
+
+import Foundation
+
+private extension String {
+    var nsString: NSString {
+        return NSString(string: self)
+    }
+    var fileName: String {
+        return nsString.lastPathComponent
+    }
+    var fileNameWithoutExtension: String {
+        return fileName.nsString.stringByDeletingPathExtension
+    }
+}
+
+public class LogUtils {
+    public static func println(items: Any, path: String = #file, line: Int = #line, function: String = #function) {
+        print(path.fileNameWithoutExtension, terminator: ":")
+        print(line, terminator: ":")
+        print(function, terminator: ": ")
+        print(items)
+        print()
+    }
+}
