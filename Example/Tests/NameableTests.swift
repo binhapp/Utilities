d7022f12f8b4699ed6b6a12cb8ec25527d2ad0ac
blcsntb <blcsntb@gmail.com>
2017-03-11 17:08:14 +0700

    configure unit test, test case for nameable protocol

--- Example/Tests/NameableTests.swift
+++ Example/Tests/NameableTests.swift
@@ -3,12 +3,60 @@ import Utilities
 
 class NameableTests: XCTestCase {
     func testClass() {
-        class Class1: Nameable {}
         XCTAssertEqual(Class1.name, "Class1")
+        XCTAssertNotEqual(Class2.name, "Class2")
+    }
+    
+    class Class1: Nameable {
         
-        class Class2: Nameable {
-            static var name: String { return "NameClass2" }
-        }
-        XCTAssertEqual(Class2.name, "NameClass2")
+    }
+    
+    class Class2: Nameable {
+        static var name: String { return "" }
+    }
+}
+
+extension NameableTests {
+    func testViewController() {
+        XCTAssertEqual(ViewController1.name, "ViewController1")
+        XCTAssertNotEqual(ViewController2.name, "ViewController2")
+    }
+    
+    class ViewController1: UIViewController, Nameable {
+        
+    }
+    
+    class ViewController2: UIViewController, Nameable {
+        static var name: String { return "" }
+    }
+}
+
+extension NameableTests {
+    func testTableViewCell() {
+        XCTAssertEqual(TableViewCell1.name, "TableViewCell1")
+        XCTAssertNotEqual(TableViewCell2.name, "TableViewCell2")
+    }
+    
+    class TableViewCell1: UITableViewCell, Nameable {
+        
+    }
+    
+    class TableViewCell2: UITableViewCell, Nameable {
+        static var name: String { return "" }
+    }
+}
+
+extension NameableTests {
+    func testCollectionViewCell() {
+        XCTAssertEqual(CollectionViewCell1.name, "CollectionViewCell1")
+        XCTAssertNotEqual(CollectionViewCell2.name, "CollectionViewCell2")
+    }
+    
+    class CollectionViewCell1: UICollectionViewCell, Nameable {
+        
+    }
+    
+    class CollectionViewCell2: UICollectionViewCell, Nameable {
+        static var name: String { return "" }
     }
 }
