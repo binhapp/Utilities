633e97801302d00aa33890726264578505309eb1
blcsntb <blcsntb@gmail.com>
2017-06-30 15:53:57 +0700

    Remove api structure

--- Example/Pods/Target Support Files/Pods-Utilities_Example/Pods-Utilities_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Utilities_Example/Pods-Utilities_Example-frameworks.sh
@@ -89,13 +89,9 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Utilities/Utilities.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Utilities/Utilities.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
